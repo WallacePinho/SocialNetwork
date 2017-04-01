0ed88de9b071d906eb7815bcf82d9d028a945875
Wallace Pinho <wallacepinho@hotmail.com>
2017-04-01 18:08:32 -0300

    Add construtor padrão

--- SocialNetwork.Web/Controllers/HomeController.cs
+++ SocialNetwork.Web/Controllers/HomeController.cs
@@ -1,17 +1,44 @@
-﻿using System;
+﻿using SocialNetwork.Web.Attributes;
+using SocialNetwork.Web.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Mvc;
 
-namespace SocialNetwork.Web.Controllers
-{
-    public class HomeController : Controller
-    {
+namespace SocialNetwork.Web.Controllers {
+    public class HomeController : Controller {
+
+        private HttpClient _client;
+        private TokenHelper _tokenHelper;
+
+        public HomeController() {
+            _client = new HttpClient();
+
+            _client.BaseAddress = new Uri("https://localhost:44323/");
+            _client.DefaultRequestHeaders.Accept.Clear();
+
+            var mediaType = new MediaTypeWithQualityHeaderValue("application/json");
+            _client.DefaultRequestHeaders.Accept.Add(mediaType);
+
+            _tokenHelper = new TokenHelper();
+        }
+
+
         // GET: Home
-        public ActionResult Index()
-        {
+        [Authentication]
+        public ActionResult Index() {
             return View();
+        }
+
+        protected override void Dispose(bool disposing) {
+            if (disposing && _client != null) {
+                _client.Dispose();
+                _client = null;
+            }
+            base.Dispose(disposing);
         }
     }
 }
